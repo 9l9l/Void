74547abd4119be6260a72c95625d8717a72cf78b
prism <snawalt420@proton.me>
2026-02-25 20:26:05 +0100

    add MessageClickActions plugin for double-click to edit

---
+++ src/plugins/messageClickActions/index.ts
@@ -0,0 +1,31 @@
+/*
+ * Void, a modification for grok.com
+ * Copyright (c) 2026 Void contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+function onDblClick(e: MouseEvent) {
+    const target = e.target as HTMLElement;
+    const response = target.closest<HTMLElement>("[id^='response-']");
+    if (!response) return;
+
+    const editBtn = response.querySelector<HTMLButtonElement>("[aria-label='Edit']");
+    editBtn?.click();
+}
+
+export default definePlugin({
+    name: "MessageClickActions",
+    description: "Double-click your own messages to edit them.",
+    authors: [Devs.Prism],
+
+    start() {
+        document.addEventListener("dblclick", onDblClick);
+    },
+
+    stop() {
+        document.removeEventListener("dblclick", onDblClick);
+    },
+});
